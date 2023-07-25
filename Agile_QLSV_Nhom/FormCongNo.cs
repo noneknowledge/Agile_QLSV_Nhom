2e019054b657f33f7a42681915b6e669759e0089
noneknowledge <hoangphuc160802@gmail.com>
2023-07-25 17:37:07 +0700

    them form công nợ

---
+++ Agile_QLSV_Nhom/FormCongNo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab7___ADO.NET
+{
+    public partial class FormCongNo : Form
+    {
+        string masv;
+        public FormCongNo(string MSSV = null)
+        {
+            InitializeComponent();
+            masv = MSSV == null ? "20001" : MSSV;
+            
+        }
+        
+        QLHSDataContext db = new QLHSDataContext();
+
+        private void FormCongNo_Load(object sender, EventArgs e)
+        {
+            var dkhp = db.DKHPs.Where(a => a.MASV == masv).Select(a => new {a.MALHP, a.HOCPHI, a.TTRANGHP });
+            var lophp = db.LOPHPs;
+            var hocphan = db.HOCPHANs;
+            var TenHocPhan = lophp.Join(hocphan, a => a.MAHP, b => b.MAHP, (a, b) => new { a.MALHP, b.TENHP });
+            var tenhpCuaHs = dkhp.Join(TenHocPhan, a => a.MALHP, b => b.MALHP, (a, b) => new { a.MALHP, b.TENHP, a.HOCPHI, a.TTRANGHP });
+            dataGridView1.DataSource = tenhpCuaHs;
+            int ?Tong = tenhpCuaHs.Sum(a => a.HOCPHI);
+            int ?DaNop = tenhpCuaHs.Where(a =>a.TTRANGHP == "da thu").Sum(a => a.HOCPHI);
+            int? no = Tong - DaNop;
+            lblNo.Text = no.ToString() + " Đ";
+            lblTong.Text = Tong.ToString() + " Đ";
+            lblNop.Text = DaNop.ToString() + " Đ";
+
+
+
+        }
+    }
+}
---
+++ Agile_QLSV_Nhom/FormCongNo.designer.cs
@@ -0,0 +1,154 @@
+﻿namespace lab7___ADO.NET
+{
+    partial class FormCongNo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblNo = new System.Windows.Forms.Label();
+            this.lblTong = new System.Windows.Forms.Label();
+            this.lblNop = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(634, 258);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(0, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Tiền tổng các môn";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Location = new System.Drawing.Point(6, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Đã nộp";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label4.Location = new System.Drawing.Point(6, 106);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Tiền nợ";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblNo);
+            this.groupBox1.Controls.Add(this.lblTong);
+            this.groupBox1.Controls.Add(this.lblNop);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(12, 296);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(634, 142);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            // 
+            // lblNo
+            // 
+            this.lblNo.AutoSize = true;
+            this.lblNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblNo.Location = new System.Drawing.Point(397, 106);
+            this.lblNo.Name = "lblNo";
+            this.lblNo.Size = new System.Drawing.Size(61, 20);
+            this.lblNo.TabIndex = 7;
+            this.lblNo.Text = "Tiền nợ";
+            // 
+            // lblTong
+            // 
+            this.lblTong.AutoSize = true;
+            this.lblTong.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblTong.Location = new System.Drawing.Point(397, 22);
+            this.lblTong.Name = "lblTong";
+            this.lblTong.Size = new System.Drawing.Size(139, 20);
+            this.lblTong.TabIndex = 5;
+            this.lblTong.Text = "Tiền tổng các môn";
+            // 
+            // lblNop
+            // 
+            this.lblNop.AutoSize = true;
+            this.lblNop.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblNop.Location = new System.Drawing.Point(397, 65);
+            this.lblNop.Name = "lblNop";
+            this.lblNop.Size = new System.Drawing.Size(61, 20);
+            this.lblNop.TabIndex = 6;
+            this.lblNop.Text = "Đã nộp";
+            // 
+            // FormCongNo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(658, 471);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormCongNo";
+            this.Text = "FormCongNo";
+            this.Load += new System.EventHandler(this.FormCongNo_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblNo;
+        private System.Windows.Forms.Label lblTong;
+        private System.Windows.Forms.Label lblNop;
+    }
+}
