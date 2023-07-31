3f6dc3872a414ce1822538bedc6a45d55febc32b
Phuc Nguyen <hoangphuc160802@gmail.com>
2023-07-31 21:09:09 +0700

    cập nhật start-up item

--- lab7 - ADO.NET/lab7 - ADO.NET/Program.cs
+++ lab7 - ADO.NET/lab7 - ADO.NET/Program.cs
@@ -16,7 +16,7 @@ namespace lab7___ADO.NET
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormLop());
+            Application.Run(new Form1());
         }
     }
 }
